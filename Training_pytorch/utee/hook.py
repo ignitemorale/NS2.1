3e2040b5d22ae3c0f02a1db3620d08d044421f6c
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-05-01 04:45:05 +0900

    Update hook.py

--- Training_pytorch/utee/hook.py
+++ Training_pytorch/utee/hook.py
@@ -36,7 +36,7 @@ def write_matrix_weight(input_matrix,filename):
     np.savetxt(filename, weight_matrix, delimiter=",",fmt='%10.5f')
 
 def write_matrix_activation_conv(input_matrix,fill_dimension,length,filename):
-    filled_matrix_b = np.zeros([input_matrix.shape[2],input_matrix.shape[1]*length],dtype=np.str)
+    filled_matrix_b = np.zeros([input_matrix.shape[2],input_matrix.shape[1]*length],dtype=str)
     filled_matrix_bin,scale = dec2bin(input_matrix[0,:],length)
     for i,b in enumerate(filled_matrix_bin):
         filled_matrix_b[:,i::length] =  b.transpose()
