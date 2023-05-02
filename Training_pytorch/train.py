04e70c900d1739db4975ed851640bcd558287405
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-05-02 20:55:59 +0900

    Update train.py

--- Training_pytorch/train.py
+++ Training_pytorch/train.py
@@ -172,7 +172,7 @@ try:
             # model.apply(weightclip)
             j=0
             for name, param in list(model.named_parameters())[::-1]:
-                weightclip(pram.data)
+                weightclip(param.data)
                 velocity[j] = gamma * velocity[j] + alpha * param.grad.data
                 param.grad.data = velocity[j]
                 param.grad.data = wage_quantizer.QG(param.data,args.wl_weight,param.grad.data,args.wl_grad,grad_scale,
