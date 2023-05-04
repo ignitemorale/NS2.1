6ff58a804c7c546b45045217ed0bb86b823021ed
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-05-04 22:37:37 +0900

    Create plots.py

---
+++ Training_pytorch/utee/plots.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def plots(data, LTP, LTD):
+
+    df = pd.read_csv(data + '.csv')
+
+    x = df.iloc[:, 0]
+    y = df.iloc[:, 2]
+
+    plt.plot(x, y, 'o-', markersize=3)
+
+    plt.title('LTP ' + LTP + ' / ' + 'LTD ' + LTD)
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+
+    plt.savefig(data + '_' + LTP + '_' + LTD + '.png', dpi=300)
+
+'''
+data = str(input('Input data name: '))
+LTPn = str(input('Input LTP: '))
+LTDn = str(input('Input LTD: '))
+plots(data, LTPn, LTDn)
+'''
